74b2346aba58d2a02929487aeb7207d16069719a
Mehdi <mehdi-bs@live.fr>
2019-03-21 23:19:05 +0100

    Update Code.R

--- Code.R
+++ Code.R
@@ -36,27 +36,39 @@ sup_98 <- (maga_98+na98)/nrow(MAGANewsData) #34.22%
 sup_03 <- (maga_03+na03)/nrow(MAGANewsData) #58.9%
 
 
-
-#Question 2
-control <- MAGANewsData[MAGANewsData$maganews1998 == 0 & MAGANewsData$maganews2000 == 0,]
-treated <- MAGANewsData[MAGANewsData$maganews1998 == 0 & MAGANewsData$maganews2000 == 1,]
+#Question 2 
+control <- MAGANewsData[MAGANewsData$maganews2000 == 0,]
+treated <- MAGANewsData[MAGANewsData$maganews2000 == 1,]
 
 diffindiff <- (mean(treated$reppresfv2p2000,na.rm = TRUE)-mean(treated$reppresfv2p1996, na.rm = TRUE)) - (mean(control$reppresfv2p2000, na.rm = TRUE)-mean(control$reppresfv2p1996, na.rm = TRUE))
 diffindiff
 
 copy <- MAGANewsData[,c("maganews1998","maganews2000","reppresfv2p2000","reppresfv2p1996")]
 
+#dataset pour la regression du diffindiff qui permettra d'obtenir estimateur et variance
+pane <- rbind(copy[,c("reppresfv2p1996","maganews1998")],setNames(copy[,c("reppresfv2p2000","maganews2000")],c("reppresfv2p1996","maganews1998")))
+period <- rep(0,nrow(pane))
+period[(nrow(copy)+1):nrow(pane)]<-1
+pane["time"] <- period
+pane["treated"] <- c(as.numeric(copy$maganews2000 == 1),as.numeric(copy$maganews2000 == 1))
+pane["interaction"] <- pane$time * pane$treated
+
+reg <- lm(reppresfv2p1996 ~time + treated + interaction, data=pane)
+summary(reg)
 
 
 
 
 #Question 3 
-#L'estimateur n'est pas crédible car... ? 
-
+#L'estimateur n'est pas crédible car... ? common trend assumption non satisfaite probablement
+#
 
 
 
 #Question 4
+#Regardons quelques statistiques descriptives pour les groupes de traitement et de controle 
+#en période 1 (en 98-96). Pour montrer que les groupes ne sont pas forcément comparable.
+
 
 
 
